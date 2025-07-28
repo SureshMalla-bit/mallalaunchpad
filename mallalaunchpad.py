6df50ab73b19ce04ee7c9f462954a44d1db4b8fd
Suresh Malla <sureshmalla589@gmail.com>
2025-07-28 20:48:29 +0530

    Fix: import pyrebase4 instead of pyrebase

--- mallalaunchpad.py
+++ mallalaunchpad.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import pyrebase
+import pyrebase4
 import datetime
 from streamlit_option_menu import option_menu
 
@@ -24,7 +24,7 @@ try:
         st.error("GEMINI_API_KEY not found in secrets.toml. AI features will fail.")
         st.stop()
 
-    firebase = pyrebase.initialize_app(firebase_config)
+    firebase = pyrebase4.initialize_app(firebase_config)
     auth = firebase.auth()
     storage = firebase.storage()
 
